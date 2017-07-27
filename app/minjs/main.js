560dd535fc5cc14025a5cd11d8537ce673bdcf0b
Mateusz Chybiorz <mateusz.chybiorz@gmail.com>
2017-07-27 22:30:36 +0200

    galleries added

--- app/minjs/main.js
+++ app/minjs/main.js
@@ -478,6 +478,24 @@
 })();
 
 /**
+ * Created by Mateusz Chybiorz on 2017-07-22.
+ */
+/**
+ * Created by Mateusz Chybiorz on 2017-03-24.
+ */
+
+(function(){
+    angular.module('geosilesia').controller('StructuresController', ['$scope', function($scope){
+        $scope.activeTab = 1;
+        $scope.checkTab = function(tab){
+            $scope.activeTab = tab;
+            console.log($scope.activeTab);
+        }
+    }]);
+})();
+
+
+/**
  * Created by Mateusz Chybiorz on 2017-07-09.
  */
 (function(){
@@ -688,24 +706,6 @@
         ],{name: 'Custom'})
     );
 })();
-
-/**
- * Created by Mateusz Chybiorz on 2017-07-22.
- */
-/**
- * Created by Mateusz Chybiorz on 2017-03-24.
- */
-
-(function(){
-    angular.module('geosilesia').controller('StructuresController', ['$scope', function($scope){
-        $scope.activeTab = 1;
-        $scope.checkTab = function(tab){
-            $scope.activeTab = tab;
-            console.log($scope.activeTab);
-        }
-    }]);
-})();
-
 
 /**
  * Created by Mateusz Chybiorz on 2017-07-23.
