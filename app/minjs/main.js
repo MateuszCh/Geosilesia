7ad4d87eefbe8ee6940b2aef5179afcfdbbd7ef7
Mateusz Chybiorz <mateusz.chybiorz@gmail.com>
2017-07-30 17:30:54 +0200

    fullScreenMode rwd

--- app/minjs/main.js
+++ app/minjs/main.js
@@ -542,6 +542,24 @@
 })();
 
 /**
+ * Created by Mateusz Chybiorz on 2017-07-22.
+ */
+/**
+ * Created by Mateusz Chybiorz on 2017-03-24.
+ */
+
+(function(){
+    angular.module('geosilesia').controller('StructuresController', ['$scope', function($scope){
+        $scope.activeTab = 1;
+        $scope.checkTab = function(tab){
+            $scope.activeTab = tab;
+            console.log($scope.activeTab);
+        }
+    }]);
+})();
+
+
+/**
  * Created by Mateusz Chybiorz on 2017-07-09.
  */
 (function(){
@@ -752,24 +770,6 @@
         ],{name: 'Custom'})
     );
 })();
-
-/**
- * Created by Mateusz Chybiorz on 2017-07-22.
- */
-/**
- * Created by Mateusz Chybiorz on 2017-03-24.
- */
-
-(function(){
-    angular.module('geosilesia').controller('StructuresController', ['$scope', function($scope){
-        $scope.activeTab = 1;
-        $scope.checkTab = function(tab){
-            $scope.activeTab = tab;
-            console.log($scope.activeTab);
-        }
-    }]);
-})();
-
 
 /**
  * Created by Mateusz Chybiorz on 2017-07-23.
