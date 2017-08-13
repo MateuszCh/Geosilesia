fb027597fa44d91ebdac4a174a98ebf7388f5b84
Mateusz Chybiorz <mateusz.chybiorz@gmail.com>
2017-08-14 00:43:52 +0200

    bannerPhoto height

--- app/js/components/Homepage.component.js
+++ app/js/components/Homepage.component.js
@@ -19,18 +19,23 @@
         vm.$onInit = onInit;
         vm.$onDestroy = onDestroy;
         var mainHeader = document.getElementById("mainHeader");
-
-        vm.scrollTo
+        var heightInner;
 
         function onDestroy(){
             $rootScope.hideHeader = false;
             $window.removeEventListener('scroll', hideHeader);
         }
 
+        function setBannerHeight(){
+            heightInner = $window.innerHeight;
+            vm.bannerHeight = heightInner + 'px';
+        }
+
         function onInit(){
             $http.get("json/markers.json").then(function (response) {
                 vm.markers = response.data.obiekty;
             });
+            setBannerHeight();
 
             if(document.getElementById('map')){
                 $window.addEventListener('scroll', hideHeader);
@@ -40,7 +45,7 @@
         function hideHeader(){
             if(mainHeader){
                 var sizeOfHeader = mainHeader.offsetHeight;
-                var heightInner = $window.innerHeight;
+                setBannerHeight();
                 var scrollTopWindow = $window.scrollY;
                 var diff = scrollTopWindow + sizeOfHeader;
                 if(heightInner < diff){
