ceea36df66ce0ab4b36e56ab90c0a2b6613b6617
Mateusz Chybiorz <mateusz.chybiorz@gmail.com>
2017-08-13 02:23:16 +0200

    fullScreenMode mobile fix

--- app/minjs/main.js
+++ app/minjs/main.js
@@ -788,24 +788,6 @@
 })();
 
 /**
- * Created by Mateusz Chybiorz on 2017-07-22.
- */
-/**
- * Created by Mateusz Chybiorz on 2017-03-24.
- */
-
-(function(){
-    angular.module('geosilesia').controller('StructuresController', ['$scope', function($scope){
-        $scope.activeTab = 1;
-        $scope.checkTab = function(tab){
-            $scope.activeTab = tab;
-            console.log($scope.activeTab);
-        }
-    }]);
-})();
-
-
-/**
  * Created by Mateusz Chybiorz on 2017-07-23.
  */
 (function(){
@@ -822,3 +804,20 @@
         }
     });
 })();
+/**
+ * Created by Mateusz Chybiorz on 2017-07-22.
+ */
+/**
+ * Created by Mateusz Chybiorz on 2017-03-24.
+ */
+
+(function(){
+    angular.module('geosilesia').controller('StructuresController', ['$scope', function($scope){
+        $scope.activeTab = 1;
+        $scope.checkTab = function(tab){
+            $scope.activeTab = tab;
+            console.log($scope.activeTab);
+        }
+    }]);
+})();
+
