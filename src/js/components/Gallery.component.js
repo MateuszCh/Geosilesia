5ab360cef5fea48eb0511a4664a4df1d04d5602b
Mateusz Chybiorz <mateusz.chybiorz@gmail.com>
2017-12-25 23:00:27 +0100

    Gallery firefox fix

--- src/js/components/Gallery.component.js
+++ src/js/components/Gallery.component.js
@@ -1,31 +1,91 @@
 (function(){
     angular.module('geosilesia').component('gallery', {
         templateUrl: 'html/views/gallery.html',
-        bindings: {
-            custom: '<'
-        },
         controllerAs: 'vm',
         controller: GalleryController
     });
 
-    GalleryController.$inject = ['$http', '$routeParams', '$rootScope'];
+    GalleryController.$inject = ['$http', '$routeParams', '$rootScope', '$timeout'];
 
-    function GalleryController($http, $routeParams, $rootScope){
+    function GalleryController($http, $routeParams, $rootScope, $timeout){
         var vm = this;
         vm.$onInit = onInit;
         vm.showFullMode = false;
         vm.openFullMode = openFullMode;
-
-        function openFullMode(img) {
-            vm.showFullMode = true;
-            vm.openWith = img;
-            $rootScope.blur = true;
-        }
+        vm.closeFullScreenMode = closeFullScreenMode;
+        vm.next = next;
+        vm.prev = prev;
 
         function onInit() {
             $http.get("json/galleries/" + $routeParams.galleryId + ".json").then(function (gallery) {
                 vm.gallery = gallery.data;
+                vm.numberOfImages = gallery.data.images.length;
             });
+        }
+
+        function openFullMode(img) {
+            vm.noMove = true;
+            vm.currentImage = img;
+            $rootScope.blur = true;
+            vm.nextImage = vm.currentImage + 1;
+            vm.prevImage = vm.currentImage - 1;
+            vm.show = true;
+        }
+
+        function closeFullScreenMode(){
+            $rootScope.blur = false;
+            vm.show = false;
+
+            $timeout(function(){
+                vm.currentImage = undefined;
+                vm.nextImage = undefined;
+                vm.prevImage = undefined;
+            }, 500);
+        }
+
+        function next(){
+            vm.back = false;
+            $timeout(function(){
+                vm.noMove = false;
+                if((vm.currentImage + 1) === vm.numberOfImages){
+                    vm.currentImage = 0;
+                    vm.nextImage = 1;
+                    vm.prevImage = vm.numberOfImages - 1;
+                } else {
+                    vm.currentImage++;
+                    if((vm.currentImage + 1) === vm.numberOfImages){
+                        vm.nextImage = 0;
+                        vm.prevImage = vm.numberOfImages - 2;
+                    } else {
+                        vm.nextImage = vm.currentImage + 1;
+                        vm.prevImage = vm.currentImage - 1;
+                    }
+                }
+            }, 100);
+
+        }
+
+        function prev(){
+            vm.back = true;
+            vm.noMove = true;
+            $timeout(function(){
+                vm.noMove = false;
+                if(vm.currentImage === 0){
+                    vm.currentImage = (vm.numberOfImages - 1);
+                    vm.nextImage = 0;
+                    vm.prevImage = vm.currentImage - 1;
+                } else {
+                    vm.currentImage--;
+                    if(vm.currentImage === 0){
+                        vm.nextImage = 1;
+                        vm.prevImage = (vm.numberOfImages - 1);
+                    } else {
+                        vm.nextImage = vm.currentImage + 1;
+                        vm.prevImage = vm.currentImage - 1;
+                    }
+
+                }
+            }, 100);
         }
     }
 })();
