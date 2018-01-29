826861940f792dee07075968bf313d3f17dfcdb1
MateuszCh <mateusz.chybiorz@gmail.com>
2018-01-29 16:21:41 +0100

    buttonUp component

--- app/front/src/js/controllers/Main.controller.js
+++ app/front/src/js/controllers/Main.controller.js
@@ -1,19 +1,9 @@
 (function(){
-
     angular.module('geosilesia').controller('MainController', ['$scope', '$window', '$rootScope', function($scope, $window, $rootScope){
-
-        $window.addEventListener('scroll', showButtonUp);
-
-        function showButtonUp(){
-            this.pageYOffset >= 100 ? $scope.showButtonUp = true : $scope.showButtonUp = false;
-            $scope.$apply();
-        }
-
         $rootScope.$on("$routeChangeSuccess", function(){
             $window.scrollTo(0,0);
             $rootScope.blur = false;
         });
-
     }]);
 })();
 
