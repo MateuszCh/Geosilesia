110676c8874508f3677b63a31cbbde150e208263
MateuszCh <mateusz.chybiorz@gmail.com>
2018-02-28 23:28:07 +0100

    basic pages and page

--- app/frodo/front/src/js/components/pages/Page.component.js
+++ app/frodo/front/src/js/components/pages/Page.component.js
@@ -2,12 +2,107 @@
     angular.module('frodo').component('page', {
         templateUrl: 'html/components/pages/page.html',
         controllerAs: 'vm',
+        bindings: {
+            edit: '<'
+        },
         controller: PageController
     });
 
-    PageController.$inject = [];
-    function PageController(){
+    PageController.$inject = ['$scope', '$compile', 'pagesService', '$rootScope', '$location', '$timeout', '$routeParams', 'tools'];
+    function PageController($scope, $compile, pagesService, $rootScope, $location, $timeout, $routeParams, tools){
         var vm  = this;
+        var resultTimeout;
+        // var fieldsElement = angular.element(document.querySelector('#postFields'));
+        vm.$onInit = onInit;
+        vm.save = save;
+        vm.remove = remove;
+        // vm.addField = addField;
+        // vm.formatTypeString = formatTypeString;
+
+        vm.actionStatus = {
+            busy: false,
+            result: "",
+            status: undefined,
+        };
+
+        vm.model = {
+            title: '',
+            url: '',
+            rows: []
+        };
+
+        function onInit(){
+            if(vm.edit){
+                pagesService.getById($routeParams.id)
+                    .then(function(response){
+                        if(!response.data){
+                            $location.path('/pages');
+                        } else {
+                            vm.model = response.data;
+                            vm.currentTitle = vm.model.title;
+                        }
+                    })
+                    .catch(function(){
+                        $location.path('/pages');
+                    })
+            }
+        }
+
+        // function addField(){
+        //     var html = '<add-field model="vm.model.fields"></add-field>';
+        //     var newField = $compile(html)($scope);
+        //     fieldsElement.append(newField);
+        // }
+        //
+        // function formatTypeString(){
+        //     if(vm.model.type){
+        //         vm.model.type = vm.model.type.replace(/\s+/g, "_").toLowerCase();
+        //     }
+        // }
+
+        function save(){
+            $timeout.cancel(resultTimeout);
+            setActionStatus('save');
+            var promise = vm.edit ? pagesService.edit(vm.model) : pagesService.create(vm.model);
+
+            promise
+                .then(function(response){
+                    if(vm.edit){
+                        vm.model = response.data;
+                        vm.currentTitle = vm.model.title;
+                        setActionStatus(false, vm.model.title +  " page updated successfully", response.status);
+                        resultTimeout = $timeout(setActionStatus, 10000);
+                    } else {
+                        $location.path('/pages');
+                    }
+                })
+                .catch(function(err){
+                    setActionStatus(false, err.data.error, err.status);
+                    resultTimeout = $timeout(setActionStatus, 10000);
+                })
+        }
+
+        function remove(){
+            $timeout.cancel(resultTimeout);
+            setActionStatus('remove');
+            pagesService.remove(vm.model._id)
+                .then(function(){
+                    setActionStatus();
+                    $location.path('/pages');
+                })
+                .catch(function(err){
+                    setActionStatus(false, 'There was error removing ' + vm.model.title + ' page.', err.status);
+                    resultTimeout = $timeout(setActionStatus, 10000);
+                })
+        }
+
+        function setActionStatus(type, result, status){
+            vm.actionStatus = {
+                busyType: type || false,
+                result: result || "",
+                status: status || undefined
+            }
+        }
 
     }
 })();
--- app/frodo/front/src/js/components/pages/PagesListing.component.js
+++ app/frodo/front/src/js/components/pages/PagesListing.component.js
@@ -5,9 +5,56 @@
         controller: PagesListingController
     });
 
-    PagesListingController.$inject = [];
-    function PagesListingController(){
+    PagesListingController.$inject = ['pagesService', '$rootScope', '$timeout'];
+    function PagesListingController(pagesService, $rootScope, $timeout){
         var vm  = this;
+        vm.$onInit = onInit;
+        vm.removePage = removePage;
+        vm.loadingPages = true;
+        vm.removeStatus = {
+            busy: false,
+            result: "",
+            status: undefined,
+        };
+        var resultTimeout;
 
+        function onInit(){
+            getPages();
+        }
+
+        function removePage(id){
+            $timeout.cancel(resultTimeout);
+            setRemoveStatus(id);
+            pagesService.remove(id)
+                .then(function(response){
+                    setRemoveStatus(id, response.data, response.status);
+                    resultTimeout = $timeout(setRemoveStatus, 10000);
+                    getPages();
+                })
+                .catch(function(err){
+                    setRemoveStatus(false, err.data.error, err.status);
+                    resultTimeout = $timeout(setRemoveStatus, 10000);
+                })
+        }
+
+        function getPages(){
+            vm.loadingPages = true;
+            pagesService.getAll()
+                .then(function(response){
+                    vm.pages = response.data;
+                    vm.loadingPages = false;
+                })
+                .catch(function(){
+                    vm.loadingPages = false;
+                })
+        }
+
+        function setRemoveStatus(id, result, status){
+            vm.removeStatus = {
+                busy: id || false,
+                result: result || "",
+                status: status || undefined
+            };
+        }
     }
 })();
