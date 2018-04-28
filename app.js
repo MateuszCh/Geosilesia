b11bea0911e3401936be3a5e1a687469f2470a64
MateuszCh <mateusz.chybiorz@gmail.com>
2018-04-28 02:33:53 +0200

    mongodb connection, posts, pages

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
 const express = require('express'),
       bodyParser = require('body-parser'),
-      path = require('path');
+      path = require('path'),
+      MongoClient = require('mongodb').MongoClient,
+      config = require('./config');
 
 const app = express();
+app.set('port', process.env.PORT || 3001);
+
+let db;
+const collections = {};
+
+MongoClient.connect(config.mongoUrl)
+    .then(client => {
+        db = client.db('geosilesia');
+        collections.posts = db.collection('posts');
+        collections.pages = db.collection('pages');
+        app.listen(app.get('port'), () => console.log("Running on port 3001"));
+
+    })
+    .catch(err => {
+        throw err;
+    });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.get('/api/posts/:type', (req, res, next) => {
+    collections.posts.find({type: req.params.type}).toArray()
+        .then(posts => res.send(posts))
+        .catch(next);
+});
+
+app.get('/api/page/:pageUrl', (req, res, next) => {
+   collections.pages.find({pageUrl: req.params.pageUrl}).toArray()
+       .then(page => res.send(page))
+       .catch(next);
+});
+
 app.use('/uploads', express.static(`${__dirname}/uploads`));
 app.use('/', express.static(`${__dirname}/front/public`));
 app.get(['*'], (req, res) => res.sendFile(path.resolve(`${__dirname}/front/public/index.html`)));
-
-app.set('port', process.env.PORT || 3001);
 
 app.use((err, req, res, next) => {
     console.log(err);
@@ -28,4 +56,3 @@ app.use((err, req, res, next) => {
     }
 });
 
-app.listen(app.get('port'), () => console.log("Running on port 3001"));
