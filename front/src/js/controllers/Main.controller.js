61eac4ce68ca9d011d8fb97435bdb1cac506e8d7
MateuszCh <mateusz.chybiorz@gmail.com>
2018-04-28 11:40:19 +0200

    fe amends

--- front/src/js/controllers/Main.controller.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    angular.module('geosilesia').controller('MainController', ['$window', '$rootScope', function($window, $rootScope){
-        $rootScope.$on("$routeChangeSuccess", function(){
-            $window.scrollTo(0,0);
-            $rootScope.blur = false;
-        });
-    }]);
-})();
-
