c7839346ec599a4ace7fde7a937955ba46153b1b
MateuszCh <mateusz.chybiorz@gmail.com>
2018-05-06 10:55:09 +0200

    old files removed

--- front/src/js/controllers/geoslask.controller.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    angular.module('geosilesia').controller('GeoslaskController', ['$scope', function($scope){
-        $scope.activeTab = 1;
-        $scope.checkTab = function(tab){
-            $scope.activeTab = tab;
-        }
-    }]);
-})();
-
