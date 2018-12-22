88d4cf6652171bd9ee712bd5ebfe3d7146f97490
MateuszCh <mateusz.chybiorz@gmail.com>
2018-12-22 12:31:15 +0100

    sw v

--- front/public/sw.js
+++ front/public/sw.js
@@ -1,7 +1,7 @@
 importScripts("/js/idb.js");
 
-var CACHE_STATIC_NAME = "static-v15";
-var CACHE_DYNAMIC_NAME = "dynamic-v15";
+var CACHE_STATIC_NAME = "static-v16";
+var CACHE_DYNAMIC_NAME = "dynamic-v16";
 var STATIC_FILES = [
     "/",
     "index.html",
